7af11d3c69f250029c2498fd812acf8abaf41f0f
ahadsheriff <ahadsheriff@gmail.com>
2016-02-18 22:46:18 -0500

    Created minus button

--- WaterCrunch/PushButtonView.swift
+++ WaterCrunch/PushButtonView.swift
@@ -11,6 +11,9 @@ import UIKit
 @IBDesignable
 
 class PushButtonView: UIButton {
+    
+    @IBInspectable var fillColor: UIColor = UIColor.grayColor()
+    @IBInspectable var isAddButton: Bool = true
 
     /*
     // Only override drawRect: if you perform custom drawing.
@@ -23,8 +26,47 @@ class PushButtonView: UIButton {
     
     override func drawRect(rect: CGRect) {
         let path = UIBezierPath(ovalInRect: rect)
-        UIColor.greenColor().setFill()
+        fillColor.setFill()
         path.fill()
+        
+        //set up the width and height variables
+        //for the horizontal stroke
+        let plusHeight: CGFloat = 3.0
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        //create the path
+        let plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = plusHeight
+        
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2 - plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2 + plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+
+        if isAddButton {
+            plusPath.moveToPoint(CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 - plusWidth/2 + 0.5))
+        
+            //add the end point to the vertical stroke
+            plusPath.addLineToPoint(CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 + plusWidth/2 + 0.5))
+        }
+        //set the stroke color
+        UIColor.whiteColor().setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
         
     }
 
