f1667a3abba14a6ac9bb996d81d1a628f62affc3
ahadsheriff <ahadsheriff@gmail.com>
2016-02-21 16:17:50 -0500

    Building blocks for data visualization
    Made a graph view nested inside a container so that I can implement a
    feature that lets the user toggle between the counter and a graph of
    their water drinking history

---
+++ WaterCrunch/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  WaterCrunch
+//
+//  Created by Ahad Sheriff on 2/21/16.
+//  Copyright © 2016 Ahad Sheriff. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
